731938fa717a95ba1ceea3428c6450a0cc1407f5
Ignacio Alvarez <ignacionicolasalvarez@gmail.com>
2020-11-18 23:20:58 -0300

    cambio de main

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-import uvicorn
+from fastapi import FastAPI
 
-if __name__ == "__main__":
-    uvicorn.run("server.app:app")
+app = FastAPI()
+
+
+@app.get("/", tags=["Root"])
+async def read_root():
+    return {"message": "Welcome to this fantastic app!"}
