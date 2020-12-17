3c10228e5bc97cbbbd0b566135f06db5422066ef
Ignacio Alvarez <ignacionicolasalvarez@gmail.com>
2020-12-17 00:13:20 -0300

    se agregó cors

--- main.py
+++ main.py
@@ -1,11 +1,29 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends
+from fastapi.middleware.cors import CORSMiddleware
+from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
+
 from server.routes.aviso import router as AvisoRouter
 
+
 app = FastAPI()
+
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
+
 
 app.include_router(AvisoRouter, tags=["Aviso"], prefix="/aviso")
 
 
 @app.get("/", tags=["Root"])
-async def read_root():
-    return {"message": "Oh what a day what a lovely day to eat a milanesa!"}
+async def Saludar():
+    return {"message": "Hola!"}
+
